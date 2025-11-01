b610b6c00dbfef4ab4caf4b24cd979f419915e08
Unto Ahti <developer.hekinav@gmail.com>
2025-11-01 14:36:21 +0200

    textures and other small improvements

--- src/graveCollection.ts
+++ src/graveCollection.ts
@@ -5,14 +5,14 @@ export interface Grave {
 
 const graves = new Array<Grave>()
 
-export function createGraves(width: number, graveYard: HTMLDivElement): void {
+export function createGraves(width: number, graveYard: HTMLDivElement, onGraveClick: Function): void {
     // height is half of width
     for (let i = 0; i < width * 2 / 3; i++) {
         for (let j = 0; j < width; j++) {
 
             const flipped = i % 3 == 2
 
-            const elem = i % 3 == 1 ? j == 0 ? pathElement(graveYard) : null : graveElement(graveYard, flipped)
+            const elem = i % 3 == 1 ? j == 0 ? pathElement(graveYard) : null : graveElement(graveYard, flipped, onGraveClick)
             if (elem) graves.push({
                 elem: elem
             })
@@ -21,7 +21,7 @@ export function createGraves(width: number, graveYard: HTMLDivElement): void {
     graveYard.style.setProperty("--size", width.toString())
 
 }
-function graveElement(container: Element, flipped: boolean): Element {
+function graveElement(container: Element, flipped: boolean, onclick: Function): Element {
     const graveTemplate = document.querySelector("#grave-template")
     if (!graveTemplate) throw new Error("Grave Tempate not found")
 
@@ -29,7 +29,9 @@ function graveElement(container: Element, flipped: boolean): Element {
     el.innerHTML = graveTemplate.innerHTML
     el.classList.add("grave")
 
-    el.addEventListener("click", e => onGraveClick(e, el))
+    el.addEventListener("click", e => onclick(e, el))
+
+    el.style.setProperty("--random", Math.floor(Math.random() * 100).toString())
 
     if (flipped) el.classList.add("flipped")
 
@@ -37,16 +39,7 @@ function graveElement(container: Element, flipped: boolean): Element {
 
     return el
 }
-function onGraveClick(_event: MouseEvent, element: HTMLDivElement) {
-    const slab = element.querySelector(".grave-slab")
-    if (slab?.classList.contains("open")) {
-        slab.classList.add("closed")
-        slab.classList.remove("open")
-    } else {
-        slab?.classList.remove("closed")
-        slab?.classList.add("open")
-    }
-}
+
 function pathElement(container: Element): Element {
 
     let el = document.createElement("div")
--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,15 @@ import { createGraves } from "./graveCollection";
 const graveYard = document.querySelector("div#graveyard")
 
 if (!graveYard) throw new Error("Graveyard element not found")
-else createGraves(8, graveYard)
+else createGraves(8, graveYard, onGraveClick)
+
+function onGraveClick(_event: MouseEvent, element: HTMLDivElement) {
+    const slab = element.querySelector(".grave-slab")
+    if (slab?.classList.contains("open")) {
+        slab.classList.add("closed")
+        slab.classList.remove("open")
+    } else {
+        slab?.classList.remove("closed")
+        slab?.classList.add("open")
+    }
+}

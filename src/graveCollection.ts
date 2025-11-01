c6851a55511d59a381fff296c418fab244af19b1
Unto Ahti <developer.hekinav@gmail.com>
2025-11-01 09:56:38 +0200

    animations

--- src/graveCollection.ts
+++ src/graveCollection.ts
@@ -13,7 +13,6 @@ export function createGraves(width: number, graveYard: HTMLDivElement): void {
             const flipped = i % 3 == 2
 
             const elem = i % 3 == 1 ? j == 0 ? pathElement(graveYard) : null : graveElement(graveYard, flipped)
-            console.log(i, j, elem)
             if (elem) graves.push({
                 elem: elem
             })
@@ -29,11 +28,24 @@ function graveElement(container: Element, flipped: boolean): Element {
     let el = document.createElement("div")
     el.innerHTML = graveTemplate.innerHTML
     el.classList.add("grave")
+
+    el.addEventListener("click", e => onGraveClick(e, el))
+
     if (flipped) el.classList.add("flipped")
 
     container.append(el)
 
     return el
+}
+function onGraveClick(_event: MouseEvent, element: HTMLDivElement) {
+    const slab = element.querySelector(".grave-slab")
+    if (slab?.classList.contains("open")) {
+        slab.classList.add("closed")
+        slab.classList.remove("open")
+    } else {
+        slab?.classList.remove("closed")
+        slab?.classList.add("open")
+    }
 }
 function pathElement(container: Element): Element {
 
